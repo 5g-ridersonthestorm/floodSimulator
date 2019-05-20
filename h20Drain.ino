bea154919d8e59bde74dfc36a520ad51fb5dda8b
James O'Sullivan <37628314+josullivanhw@users.noreply.github.com>
2019-05-20 13:28:40 +0100

    Updated before DTW2019

--- h20Drain.ino
+++ h20Drain.ino
@@ -1,12 +1,11 @@
 /*************************************************************
-  
-  Water pump controller, uses NodeMCU ESP8266 board to relay-control
-  two water pumps.
-  
+
  *************************************************************/
 
 
 #include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
 
 // Variable to store the HTTP request
 String header;
@@ -20,17 +19,20 @@ String pump1State = "off";
 String pump2State = "off";
 
 // Set web server port number to 80
-WiFiServer server(80);
+ESP8266WebServer server(80);
+
+/* Put IP Address details */
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
 
 
 // Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "James Redmi Note5";
-char pass[] = "********";
-/*
+
+
 char ssid[] = "h20-PumpServer";
 char pass[] = "f-lowh20";
-*/
+
 
 // Assign output variables to GPIO pins
 // const int relayPin1 = 5;
@@ -50,27 +52,130 @@ void setup() {
  
   
   // Connect to Wi-Fi network with SSID and password
-  Serial.print("Connecting to ");
+  Serial.print("Setting up Access Point...");
   Serial.println(ssid);
-  WiFi.begin(ssid, pass);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+
+  boolean result = WiFi.softAP(ssid, pass);
+  
+  if(result == true)
+  {
+    Serial.println("Access point ready for connections!");
+    WiFi.softAPConfig(local_ip, gateway, subnet);
+    
+    server.on("/", handle_OnConnect);
+    server.on("/pump/in", handle_pumpIn);
+    server.on("/pump/out", handle_pumpOut);
+    server.on("/pump/off", handle_pumpOff);
+    server.onNotFound(handle_NotFound);
+  
+    server.begin();
   }
-  // Print local IP address and start web server
-  Serial.println("");
-  Serial.println("WiFi connected.");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  else
+  {
+    Serial.println("Access point setup failed!");
+    delay(500);
+  }
+  // Run the web server
   server.begin();
 }
 
 
+void loop()
+{
+   server.handleClient();
+}
+
+void handle_OnConnect() {
+   pump1State = "off";
+   pump2State = "off";
+
+  Serial.println("pump1State : OFF | pump2State : OFF");
+  server.send(200, "text/html", SendHTML(pump1State,pump2State)); 
+}
 
 
-void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+void handle_pumpIn() {
+   pump1State = "on";
+   digitalWrite(relayPin1, HIGH);
+   pump2State = "off";
+   digitalWrite(relayPin2, LOW);
+   
+   Serial.println("pump1State : ON | pump2State : OFF");
+   server.send(200, "text/html", SendHTML(pump1State,pump2State)); 
+}
 
+void handle_pumpOut() {
+   pump2State = "on";
+   digitalWrite(relayPin2, HIGH);
+   pump1State = "off";
+   digitalWrite(relayPin1, LOW);
+              
+   Serial.println("pump1State : OFF | pump2State : ON");
+   server.send(200, "text/html", SendHTML(pump1State,pump2State)); 
+}
+
+
+void handle_pumpOff() {
+    pump2State = "off";
+    digitalWrite(relayPin2, LOW);
+    pump1State = "off"; 
+    digitalWrite(relayPin1, LOW);
+    Serial.println("pump1State : OFF | pump2State : OFF");
+    
+    server.send(200, "text/html", SendHTML(pump1State,pump2State)); 
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
+
+String SendHTML(String pump1State,String pump2State )
+{
+  String ptr = "<!DOCTYPE html><html>";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">";
+  ptr += "<link rel=\"icon\" href=\"data:,\">";
+  ptr += "<style>html { font-family: monospace; color:#ffffff;  display: inline-block; margin: 0px auto; text-align: center;}";
+  ptr += ".button1 { background-color: #0BFE33; border-radius: 8px; width:75%;  color: white; padding: 16px 40px; text-decoration: none; font-size: 40px; margin: 2px; cursor: pointer;}";
+  ptr += ".button2 {background-color: #ff9999; border-radius: 8px; width:75%;color: white; padding: 16px 40px; text-decoration: none; font-size: 40px; margin: 2px; cursor: pointer;}";
+  ptr += ".button3 {background-color: #FE0B0B; border-radius: 8px;width:75%;color: white; padding: 16px 40px; text-decoration: none; font-size: 40px; margin: 2px; cursor: pointer;}</style></head>";    
+         
+  // Web Page Heading
+  ptr += "<body bgcolor=\"#1a1a1a\"><h1>Water Pump Controller</h1>";
+  ptr += "<hr>";
+  ptr += "<hr>";
+  
+  // Display current state, and ON/OFF buttons for GPIO 16  
+  ptr += "<p>Left pump ";
+  ptr += pump1State;
+  ptr += "</p>";
+  ptr += "<p>Right pump ";
+  ptr += pump2State;
+  ptr += "</p>";
+            
+  // If the pump1State is off, it displays the 'Water in' button       
+  if (pump1State=="off") {
+    ptr += "<p><a href=\"/pump/in\"><button class=\"button1\">Water in</button></a></p>";
+  } 
+  else {
+    ptr += "<p><a href=\"/pump/out\"><button class=\"button2\">Water out</button></a></p>";
+  } 
+  
+  ptr += "<p><a href=\"/pump/off\"><button class=\"button3\">Off</button></a></p>";
+  ptr += "<hr>";
+  ptr += "<hr>";
+  
+  ptr += "<center><small>&copy; 5G Riders on the Storm</small></center>";
+  ptr += "</body></html>";
+            
+  return ptr;
+}
+
+
+
+/*void loop(){
+  server.handleClient();
+  
+  
   if (client) {                             // If a new client connects,
     Serial.println("New Client.");          // print a message out in the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
@@ -165,6 +270,6 @@ void loop(){
   }
 }
 
- 
+ */
 
 
